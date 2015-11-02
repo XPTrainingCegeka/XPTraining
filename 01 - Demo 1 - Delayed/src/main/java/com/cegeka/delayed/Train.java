e011aef93f8f8739f4d02bb8c7f92712daac3a5c
demo <demo@HI21253.cegekanv.corp.local>
2015-11-02 15:17:54 +0100

    up

--- 01 - Demo 1 - Delayed/src/main/java/com/cegeka/delayed/Train.java
+++ 01 - Demo 1 - Delayed/src/main/java/com/cegeka/delayed/Train.java
@@ -1,16 +1,42 @@
 package com.cegeka.delayed;
 
+import java.math.BigDecimal;
+
 public class Train {
 
-    public Train(int hoursDelayed) {
+	
+	
+    private int hoursDelayed;
 
+	public Train(int hoursDelayed) {
+		this.hoursDelayed = hoursDelayed;
     }
 
     public int getHoursDelayed() {
-
+    	return hoursDelayed;
     }
 
     public void setHoursDelayed(int hoursDelayed) {
-
+    	this.hoursDelayed = hoursDelayed;
     }
+
+	public String customerSatisfaction() {
+		// TODO Auto-generated method stub
+		String satisfaction = null;
+		switch (hoursDelayed) {
+		case 0:
+			satisfaction = "case 0";
+		case 1:
+			return "case 1";
+		case 2:
+			return "This train is annoying me!";
+		case 3:
+			satisfaction = "case 3";
+		default:
+			break;
+		}
+		return satisfaction;
+	}
+
+
 }
--- 01 - Demo 1 - Delayed/src/test/java/com/cegeka/delayed/TrainTest.java
+++ 01 - Demo 1 - Delayed/src/test/java/com/cegeka/delayed/TrainTest.java
@@ -12,5 +12,19 @@ public class TrainTest {
         Train train = new Train(2);
 
         assertThat(train.getHoursDelayed()).isEqualTo(2);
+        assertThat(train.customerSatisfaction()).isEqualTo("This train is annoying me!");
     }
+    
+    @Test
+    public void getHoursDelayed_shouldMapSatisfaction() {
+    	Train train = new Train(1);
+    	
+    	assertThat(train.getHoursDelayed()).isEqualTo(1);
+    	assertThat(train.customerSatisfaction()).isEqualTo("case 1");
+    	
+    	train.setHoursDelayed(0);
+    	assertThat(train.getHoursDelayed()).isEqualTo(1);
+    	assertThat(train.customerSatisfaction()).isEqualTo("case 0");
+    }
+    
 }
