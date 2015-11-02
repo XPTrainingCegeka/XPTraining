bbccd8e5eae33ec747a5fb13818a17b2bd0fa2e1
demo <demo@HI21261.cegekanv.corp.local>
2015-11-02 15:20:49 +0100

    implements demo 1

--- 01 - Demo 1 - Delayed/src/main/java/com/cegeka/delayed/Train.java
+++ 01 - Demo 1 - Delayed/src/main/java/com/cegeka/delayed/Train.java
@@ -1,16 +1,34 @@
 package com.cegeka.delayed;
 
+import java.math.BigDecimal;
+
 public class Train {
 
+	private int hoursDelayed;
+	
     public Train(int hoursDelayed) {
-
+    	this.hoursDelayed = hoursDelayed;
     }
 
     public int getHoursDelayed() {
-
+    	return hoursDelayed;
     }
 
     public void setHoursDelayed(int hoursDelayed) {
 
     }
+
+	public String customerSatisfaction() {
+		switch(hoursDelayed) {
+		case 0:
+			return "Nice 'n' rolling!";
+		case 1:
+			return "Just another day at the train ...";
+		case 2:
+			return "This train is annoying me!";
+		case 3:
+			return "B-u-u-urn it!";
+		}
+		return null;
+	}
 }
--- 01 - Demo 1 - Delayed/src/test/java/com/cegeka/delayed/TrainTest.java
+++ 01 - Demo 1 - Delayed/src/test/java/com/cegeka/delayed/TrainTest.java
@@ -8,9 +8,38 @@ import static org.fest.assertions.Assertions.assertThat;
 public class TrainTest {
 
     @Test
-    public void getHoursDelayed_givenTrainAndHoursDelayed_correctHoursDelayedReturned() {
+    public void getHoursDelayed_givenTrainAndHours2_correctHoursDelayedReturned() {
         Train train = new Train(2);
 
         assertThat(train.getHoursDelayed()).isEqualTo(2);
     }
+    
+    @Test
+    public void getHoursDelayed_givenTrainAndHours0_correctCustomerSatisfactionReturned() {
+    	Train train = new Train(0);
+    	
+        assertThat(train.customerSatisfaction()).isEqualTo("Nice 'n' rolling!");
+    }
+    
+    @Test
+    public void getHoursDelayed_givenTrainAndHours1_correctCustomerSatisfactionReturned() {
+    	Train train = new Train(1);
+    	
+        assertThat(train.customerSatisfaction()).isEqualTo("Just another day at the train ...");
+    }
+
+    @Test
+    public void getHoursDelayed_givenTrainAndHours2_correctCustomerSatisfactionReturned() {
+    	Train train = new Train(2);
+    	
+        assertThat(train.customerSatisfaction()).isEqualTo("This train is annoying me!");
+    }
+    
+    @Test
+    public void getHoursDelayed_givenTrainAndHours3_correctHoursDelayedReturned() {
+    	Train train = new Train(3);
+    	
+        assertThat(train.customerSatisfaction()).isEqualTo("B-u-u-urn it!");
+    }
+
 }
