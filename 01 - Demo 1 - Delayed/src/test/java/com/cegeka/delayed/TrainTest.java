e735b7438b5330a5206e85f2b2d40b27929e85b0
demo <demo@HI21262.cegekanv.corp.local>
2015-11-02 15:25:27 +0100

    First commit

--- 01 - Demo 1 - Delayed/src/test/java/com/cegeka/delayed/TrainTest.java
+++ 01 - Demo 1 - Delayed/src/test/java/com/cegeka/delayed/TrainTest.java
@@ -1,9 +1,8 @@
 package com.cegeka.delayed;
 
-import com.cegeka.delayed.Train;
-import org.junit.Test;
-
 import static org.fest.assertions.Assertions.assertThat;
+
+import org.junit.Test;
 
 public class TrainTest {
 
@@ -12,5 +11,39 @@ public class TrainTest {
         Train train = new Train(2);
 
         assertThat(train.getHoursDelayed()).isEqualTo(2);
+        assertThat(train.customerSatisfaction()).isEqualTo("This train is annoying me!");
+    }
+    
+    @Test
+    public void testThreeHours() {
+        Train train = new Train(3);
+
+        assertThat(train.getHoursDelayed()).isEqualTo(3);
+        assertThat(train.customerSatisfaction()).isEqualTo("B-u-u-urn it!");
+    }
+    
+    @Test
+    public void testOneHour() {
+        Train train = new Train(1);
+
+        assertThat(train.getHoursDelayed()).isEqualTo(1);
+        assertThat(train.customerSatisfaction()).isEqualTo("Just another day at the train …");
+    }
+    
+    
+    @Test
+    public void testZeroHour() {
+        Train train = new Train(0);
+
+        assertThat(train.getHoursDelayed()).isEqualTo(0);
+        assertThat(train.customerSatisfaction()).isEqualTo("Nice ‘n’ rolling!");
+    }
+    
+    @Test(expected = RuntimeException.class)
+    public void testFourHours() {
+        Train train = new Train(4);
+
+        assertThat(train.getHoursDelayed()).isEqualTo(4);
+        train.customerSatisfaction();
     }
 }
