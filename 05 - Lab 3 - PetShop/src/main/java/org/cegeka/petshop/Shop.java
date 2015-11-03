c90e933b10b553e366a9fb8b87cb0bf375772264
demo <demo@HI21253.cegekanv.corp.local>
2015-11-03 10:55:22 +0100

    assert size 1 of sent messages

--- 05 - Lab 3 - PetShop/src/main/java/org/cegeka/petshop/Shop.java
+++ 05 - Lab 3 - PetShop/src/main/java/org/cegeka/petshop/Shop.java
@@ -1,18 +1,31 @@
 package org.cegeka.petshop;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.mail.MessagingException;
+
 import com.google.common.collect.ImmutableSet;
 
-import java.util.*;
-
 public class Shop {
+	private EmailNotificationService service;
+	private Set<String> itemList = new HashSet<String>();
 
-    private Set<String> itemList = new HashSet<String>();
+	public Shop(EmailNotificationService notificationService) {
+		service = notificationService;
+	}
 
-    public void addItem(String item) {
-        itemList.add(item);
-    }
+	public void addItem(String item) {
+		itemList.add(item);
+		try {
+			service.sendEmail("sample", item);
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			System.err.println("Could not send email.Will skip it");
+		}
+	}
 
-    public Set<String> getItemsInStock() {
-        return ImmutableSet.copyOf(itemList);
-    }
+	public Set<String> getItemsInStock() {
+		return ImmutableSet.copyOf(itemList);
+	}
 }
--- 05 - Lab 3 - PetShop/src/test/java/org/cegeka/petshop/PetShopTest.java
+++ 05 - Lab 3 - PetShop/src/test/java/org/cegeka/petshop/PetShopTest.java
@@ -1,34 +1,57 @@
 package org.cegeka.petshop;
-import org.junit.Before;
-import org.junit.Test;
-
-import java.util.Set;
 
 import static org.fest.assertions.Assertions.assertThat;
 
+import java.util.Set;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.subethamail.wiser.Wiser;
+
 public class PetShopTest {
 
-    private Shop shop;
+	private static final int SMTP_TEST_PORT = 9000;
+	private static final String SMPT_TEST_HOST = "localhost";
+	private Shop shop;
+	private Wiser wiser;
 
-    @Before
-    public void setup()
-    {
-        shop = new Shop();
-    }
+	@Before
+	public void setup() {
+		shop = new Shop(new EmailNotificationService(SMTP_TEST_PORT, SMPT_TEST_HOST));
 
-    private void addItem(String item)
-    {
-        shop.addItem(item);
-    }
+		wiser = new Wiser();
+		wiser.setPort(SMTP_TEST_PORT);
+		wiser.setHostname(SMPT_TEST_HOST);
+		wiser.start();
+	}
 
-    @Test
-    public void addItem_whenItem_thenAddsItemToStock()
-    {
-        String item = "Doggy Gucci bag";
-        addItem(item);
+	@After
+	public void tearDown() {
+		wiser.stop();
+	}
 
-        Set<String> items = shop.getItemsInStock();
+	private void addItem(String item) {
+		shop.addItem(item);
+	}
 
-        assertThat(items.contains(item)).isTrue();
-    }
+	@Test
+	public void addItem_whenItem_thenAddsItemToStock() {
+		String item = "Doggy Gucci bag";
+		addItem(item);
+
+		Set<String> items = shop.getItemsInStock();
+
+		assertThat(items.contains(item)).isTrue();
+	}
+
+	@Test
+	public void addItem_whenItem_thenEmailIsSend() {
+		String item = "Doggy Gucci bag";
+		addItem(item);
+
+		assertThat(wiser.getMessages()).hasSize(1);
+
+	}
+
 }
