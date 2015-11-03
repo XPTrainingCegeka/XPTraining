fbe91f59f3bd3a4d966fe665569a862d2277f3f8
demo <demo@HI21253.cegekanv.corp.local>
2015-11-03 09:19:36 +0100

    Fixed midnight hunting bug

--- 03 - Lab 2 - Bugfix Dracula/src/test/java/org/cegeka/vampireslayer/HunterTest.java
+++ 03 - Lab 2 - Bugfix Dracula/src/test/java/org/cegeka/vampireslayer/HunterTest.java
@@ -1,40 +1,52 @@
 package org.cegeka.vampireslayer;
 
-import org.junit.Before;
-import org.junit.Test;
+import static org.fest.assertions.Assertions.assertThat;
 
 import java.util.Calendar;
 import java.util.Date;
 
-import static org.fest.assertions.Assertions.assertThat;
+import org.junit.Before;
+import org.junit.Test;
 
 public class HunterTest {
-    private Hunter hunter;
+	private Hunter hunter;
 
-    @Before
-    public void setup()
-    {
-        hunter = new Hunter();
-    }
+	private Date setUpDate(int hour, int minute) {
+		Calendar calendar = Calendar.getInstance();
+		Date huntingTime;
+		calendar.set(2014, Calendar.APRIL, 12, hour, minute);
+		huntingTime = calendar.getTime();
+		return huntingTime;
+	}
 
-    @Test
-    public void canHunt_givenDayTime_thenCannotHunt()
-    {
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(2014, Calendar.APRIL, 12, 16, 1);
-        Date huntingTime = calendar.getTime();
+	@Before
+	public void setup() {
+		hunter = new Hunter();
+	}
 
-        assertThat(hunter.canHunt(huntingTime)).isFalse();
-    }
+	@Test
+	public void canHunt_givenDayTime_thenCannotHunt() {
 
-    @Test
-    public void canHunt_givenNightTime_thenCanHunt()
-    {
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(2014, Calendar.APRIL, 12, 1, 1);
-        Date huntingTime = calendar.getTime();
+		Date huntingTime = setUpDate(16, 1);
 
-        assertThat(hunter.canHunt(huntingTime)).isTrue();
-    }
+		assertThat(hunter.canHunt(huntingTime)).isFalse();
+	}
 
+	@Test
+	public void canHunt_givenNightTime_thenCanHunt() {
+		Date huntingTime = setUpDate(1, 1);
+		assertThat(hunter.canHunt(huntingTime)).isTrue();
+	}
+
+	@Test
+	public void canHunt_givenMidnight_thenCanHunt() {
+		Date huntingTime = setUpDate(0, 0);
+		assertThat(hunter.canHunt(huntingTime)).isTrue();
+	}
+
+	@Test
+	public void canHunt_given6am_thenCanHunt() {
+		Date huntingTime = setUpDate(6, 0);
+		assertThat(hunter.canHunt(huntingTime)).isTrue();
+	}
 }
